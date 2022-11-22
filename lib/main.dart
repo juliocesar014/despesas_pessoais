93704d6a7264ebd00c2b87490e68c91e3f615abe
juliocesar014 <juliocesar014@live.com>
2022-11-22 19:41:41 -0300

    feat: add widget/column wrapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,42 @@
+// ignore_for_file: unused_import, unnecessary_import, use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, implementation_imports
 
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(children: <Widget>[
+        Container(
+          width: double.infinity,
+          child: Card(
+            color: Colors.blue,
+            child: Text('Gráfico'),
+            elevation: 5,
+          ),
+        ),
+        Card(
+          child: Text('Lista de Transações'),
+        )
+      ]),
+    );
+  }
+}
