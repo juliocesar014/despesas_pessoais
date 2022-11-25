5cfb4eb5ca3ea5d192507de0f74b363021b19791
juliocesar014 <juliocesar014@live.com>
2022-11-25 09:02:55 -0300

    feat: create constuctor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: unused_import, unnecessary_import, use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, implementation_imports
+// ignore_for_file: unused_import, unnecessary_import, use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, implementation_imports, avoid_unnecessary_containers
 
+import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -18,25 +19,34 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Tênis Nike', value: 310.79, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Academia', value: 99.89, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Column(children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: Card(
-            color: Colors.blue,
-            child: Text('Gráfico'),
-            elevation: 5,
-          ),
-        ),
-        Card(
-          child: Text('Lista de Transações!!!!!!'),
-        )
-      ]),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('Lista de Transações!!!!!!'),
+            )
+          ]),
     );
   }
 }
